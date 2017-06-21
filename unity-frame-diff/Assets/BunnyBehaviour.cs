5d17d51e6afe2c814c202fa8188c1675d9f80172
Jaewon Choi <jaewon.james.choi@gmail.com>
2017-06-21 16:18:18 +0900

    For backup

---
+++ unity-frame-diff/Assets/BunnyBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		angularSpeed = 0;
+	}
+
+	private float angularSpeed;
+	private bool flag = true;
+
+	float pointA = 3, pointB = -3;
+	float translateTimer = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		float dt = Time.deltaTime;
+
+		float currentPoint = pointA * translateTimer + pointB * (1 - translateTimer);
+
+		transform.position = new Vector3 (0, currentPoint, 0);
+
+		translateTimer += dt;
+
+		if (translateTimer > 1.0f) {
+			translateTimer = 0.0f;
+			float t = pointA;
+			pointA = pointB;
+			pointB = t;
+		}
+
+		transform.Rotate (Vector3.up * angularSpeed * Time.deltaTime, Space.World);
+
+		if (flag) {
+			angularSpeed += 360 * dt;
+		} else {
+			angularSpeed -= 360 * dt;
+		}
+
+		if (flag && angularSpeed > 360) {
+			flag = false;
+		} else if (!flag && angularSpeed < 0) {
+			flag = true;
+		}
+	}
+}
---
+++ unity-frame-diff/Assets/FrameDiffBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameDiffBehaviour : MonoBehaviour {
+
+	public RenderTexture renderTexture;
+
+	private Color[] lastColors;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	private Vector4 toVector4(Color color)
+	{
+		return new Vector4 (color.a, color.r, color.g, color.b);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RenderTexture.active = renderTexture;
+		Texture2D virtualPhoto = new Texture2D (renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+		virtualPhoto.ReadPixels (new Rect (0, 0, renderTexture.width, renderTexture.height), 0, 0);
+
+		Color[] currentColors = virtualPhoto.GetPixels ();
+
+		if (lastColors != null) {
+			float variantColors = 0;
+
+			for (int i = 0; i < renderTexture.width * renderTexture.height; ++i) {
+				if (!lastColors [i].Equals (currentColors [i])) {
+					variantColors += 1;
+				}
+			}
+
+			print (variantColors / renderTexture.width / renderTexture.height);
+		}
+
+		lastColors = currentColors;
+	}
+}
